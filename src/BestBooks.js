27c63f36d83242f1b8de1cce6ee5b763cd106fea
Nadeen-Shuweihat <shwueihat16@gmail.com>
2021-06-21 18:32:36 +0300

    tried

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Carousel} from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 import "./BestBooks.css";
 import axios from "axios";
 
 class BestBooks extends React.Component {
   state = {
     data: [],
+   
   };
 
   componentDidMount() {
@@ -32,24 +33,42 @@ class BestBooks extends React.Component {
 
     return (
       <>
-        <Carousel style={{width:500, height:700 , margin:'0 auto' ,backgroundColor: 'grey', marginTop:'30'}}>
-      {this.state.data.map((book)=>{
-        return (
-    
-          <Carousel.Item>
-            <img style={{width:400 ,height:650 }}
-              className="d-block w-100"
-              src={book.img}
-              alt="First slide"
-            />
-            <Carousel.Caption style={{backgroundColor: 'black' ,opacity :0.8, borderRadius:10}}>
-              <h3>{book.name}</h3>
-              <p>{book.desc}</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          );
-        })}
-        </Carousel>
+        {!this.state.data ? (
+          <p>No Books</p>
+        ) : (
+          <Carousel
+            style={{
+              width: 500,
+              height: 700,
+              margin: "0 auto",
+              backgroundColor: "grey",
+              marginTop: "30",
+            }}
+          >
+            {this.state.data.map((book) => {
+              return (
+                <Carousel.Item>
+                  <img
+                    style={{ width: 400, height: 650 }}
+                    className="d-block w-100"
+                    src={book.img}
+                    alt="First slide"
+                  />
+                  <Carousel.Caption
+                    style={{
+                      backgroundColor: "black",
+                      opacity: 0.8,
+                      borderRadius: 10,
+                    }}
+                  >
+                    <h3>{book.name}</h3>
+                    <p>{book.desc}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
+        )}
       </>
     );
   }
